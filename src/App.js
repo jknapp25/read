0f22a748f23fdf872f0b9f7198adbe17bf99dd82
Josh <joshknappp@gmail.com>
2020-08-11 13:17:22 -0700

    Three columns with markdown displaying

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { Container, Row, Col } from "react-bootstrap";
+import lorip from "./lorem-ipsum.md";
+import "bootstrap/dist/css/bootstrap.min.css";
+export default App;
+var md = require("markdown-it")();
 
 function App() {
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    fetch(lorip)
+      .then(response => response.text())
+      .then(text => {
+        setText(text);
+      });
+  }, []);
+
+  const markdown = md.render(text);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App overflow-auto">
+      <Container fluid>
+        <Row>
+          <Col />
+          <Col xs={6}>
+            <div dangerouslySetInnerHTML={{ __html: markdown }} />
+          </Col>
+          <Col />
+        </Row>
+      </Container>
     </div>
   );
 }
-
-export default App;
